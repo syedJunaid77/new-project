7c35cdeb7440e20585ceceeb3d75e1453877d2db
suhail <sayyedsuhail800900@gmail.com>
2024-05-23 18:07:24 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/src/img/background3.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
